c7ea491618c60bafc30deda6cc72c7efe6754c21
Luigi <45099401+ProfDrLuigi@users.noreply.github.com>
2024-03-13 10:12:21 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,8 +1,30 @@
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
     name: "zlib",
+    products: [
+        .library(name: "zlib", targets: ["zlib"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ProfDrLuigi/CZlib/czlib.git", from: "1.2.11")
+    ],
     targets: [
-        Target(name: "zlib-example", dependencies: ["Czlib"])
+        .target(
+            name: "zlib",
+            dependencies: ["Czlib"]
+        ),
+        .target(
+            name: "zlib-example",
+            dependencies: ["zlib"]
+        ),
+        .target(
+            name: "Czlib",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "zlibTests",
+            dependencies: ["zlib"]
+        ),
     ]
 )
